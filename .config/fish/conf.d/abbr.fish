9119f55553a7b83330dd6767bed2cf043daffb39
João Marcello <marcello.jao1611@gmail.com>
2025-06-24 16:52:13 -0300

    Add new abbr to fish

--- .config/fish/conf.d/abbr.fish
+++ .config/fish/conf.d/abbr.fish
@@ -3,3 +3,6 @@ abbr --add gcmsg git commit -m
 abbr --add gca git commit --amend --no-edit
 abbr --add gp git push
 abbr --add gl git pull
+abbr --add ga git add
+abbr --add gaa git add --all
+abbr --add gd git diff
